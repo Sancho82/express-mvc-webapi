39e3ea40a3ffca858d6b235bcc9189fc5c9112df
szani <gabor.szaniszlo82@gmail.com>
2018-12-12 16:30:11 +0100

    update route defined, errorhandling finished

--- controller/flats.js
+++ controller/flats.js
@@ -42,12 +42,30 @@ flats.post('/', (req, res) => {
 });
 
 // update
+flats.put('/:title', (req, res) => {
+  models.Flat.findOne({where: {title: req.params.title}}).then(result => {
+    if (result) {
+      models.Flat.update({
+        price: req.body.price,
+        floorArea: req.body.floorArea,
+        country: req.body.country,
+        zip: req.body.zip,
+        city: req.body.city,
+        street: req.body.street
+      }, {
+        where: {title: req.params.title}
+      }).then(res.send(`${req.params.title} has been successfully updated.`));
+    } else {
+      res.send('Sorry, there is no flat with such a title in our database at the moment.');
+    }
+  });
+});
 
 // delete (by id)
-flats.put('/:id', (req, res) => {
+flats.delete('/:id', (req, res) => {
   models.Flat.findOne({where: {id: req.params.id}}).then(result => {
     if (result) {
-      models.Flat.delete(result).then(res.send(`Flat with id: ${req.params.id} has been succesfully deleted.`));
+      models.Flat.destroy({where: {id: req.params.id}}).then(res.send(`Flat with id: ${req.params.id} has been succesfully deleted.`));
     } else {
       res.send('No house with such an id exists in database.');
     }
